9d6a28c63e6d9ca69104c27fede07b824d281f71
Daryk Kohler <daryk.kohler@gmail.com>
2021-05-17 17:14:44 -0700

    Implimented Hierarchy component to map node buttons

--- client/src/components/Hierarchy/Hierarchy.js
+++ client/src/components/Hierarchy/Hierarchy.js
@@ -1,6 +1,14 @@
+import _ from "lodash";
+
 const Hierarchy = ({ nodes, selectedNodeId, handleSelectNode }) => {
-    /* Task: Fill out Hierarchy component implementation. */
-    return null;
+  /* Task: Fill out Hierarchy component implementation. */
+  return _.map(nodes, (node) => {
+    return (
+      <button key={node.id} onClick={() => handleSelectNode(node.id)}>
+        {node.displayName}
+      </button>
+    );
+  });
 };
 
 export default Hierarchy;
