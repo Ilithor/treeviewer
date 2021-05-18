a104c5ff7398f55a94b762982e4a2bf4981f603e
Daryk Kohler <daryk.kohler@gmail.com>
2021-05-17 17:15:52 -0700

    Implimented hierarchy and linechart data handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import Hierarchy from './components/Hierarchy/Hierarchy';
-import LineChart from './components/LineChart/LineChart';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Hierarchy from "./components/Hierarchy/Hierarchy";
+import LineChart from "./components/LineChart/LineChart";
+import "./App.css";
 
 /**
  * Fill out the App.js and Hierarchy.js code so that:
@@ -8,24 +10,41 @@ import './App.css';
  * - Pass that data to the Hierarchy component to display a list of nodes
  * - When one of the nodes is clicked, fetch the node's data by calling GET /api/data/<nodeId>
  * - Pass that data to the LineChart component
-*/
+ */
 
 function App() {
+  const [hierarchyData, setHierarchyData] = useState();
+  const [lineChartData, setLineChartData] = useState();
+
   /* Task: implement handleSelectNode() */
-  const handleSelectNode = () => {}
+  const handleSelectNode = (id) => {
+    axios.get(`/api/data/${id}`).then((res) => {
+      setLineChartData(res.data);
+    });
+  };
+
+  useEffect(() => {
+    axios.get("/api/nodes").then((res) => {
+      setHierarchyData(res.data);
+    });
+  }, []);
 
   return (
     <div className="app">
       <div className="hierarchy" role="navigation">
-        <Hierarchy nodes={[/* Task: pass list of nodes to Hierarchy component */]} handleSelectNode={handleSelectNode} />
+        <Hierarchy
+          nodes={hierarchyData ? hierarchyData : []}
+          handleSelectNode={handleSelectNode}
+        />
       </div>
       <div className="main" role="main">
-        <LineChart 
-          from={'2021-01-01'}
-          to={'2021-01-10'}
+        <LineChart
+          from={"2021-01-01"}
+          to={"2021-01-10"}
           min={0}
           max={100}
-          data={[/* Task: pass node data to the LineChart component */]} />
+          data={lineChartData ? lineChartData : []}
+        />
       </div>
     </div>
   );
